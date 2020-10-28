4801481bc377fa8e270f352035b746aa79bd3182
Alexandr Tolstov <SESA574650@se.com>
2020-10-28 09:12:04 +0600

    Add project files.

---
+++ SimpleException/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        // Constant for maximum speed.
+        public const int MaxSpeed = 100;
+        // Car properties.
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+        // Is the car still operational?
+        private bool carIsDead;
+        // A car has-a radio.
+        private Radio theMusicBox = new Radio();
+        // Constructors.
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            // Delegate request to inner object.
+            theMusicBox.TurnOn(state);
+        }
+        // See if Car has overheated.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                    CurrentSpeed = 0;
+                    // Use the "throw" keyword to raise an exception.
+                    //throw new Exception($"{PetName} has overheated!");
+                    Exception ex = new Exception($"{PetName} has overheated!");
+                    ex.HelpLink = "http://www.CarsRUs.com";
+                    ex.Data.Add("TimeStamp", $"The car exploded at {DateTime.Now}");
+                    ex.Data.Add("Cause", "Yo have a lead foot.");
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
